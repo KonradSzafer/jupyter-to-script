0a295d2c95be61e6f8e58f006b92de2f07cd4944
KonradSzafer <szafer.konrad@gmail.com>
2022-06-17 22:40:53 +0200

    removing cell comments and new lines

--- convert.sh
+++ convert.sh
@@ -7,16 +7,31 @@ FILENAME="${1}"
 # convert to raw python script
 jupyter nbconvert --to script "$FILENAME"
 
-# add indents and boilerplate
+# read file
 FILENAME="${FILENAME%.*}.py"
 readarray -t file_lines < "$FILENAME"
 truncate -s 0 "$FILENAME"
 
-add_indent=False
+# add indents and boilerplate
 script=()
+add_indent=False
+skip_next=False
+cell_regex="# In[\[0-9]+\]:"
 
 for line in "${file_lines[@]}"; do
+    # remove cell beginning
+    if [[ "$line" =~ $cell_regex ]]; then
+        skip_next=True
+        continue
+    fi
+    # remove new line after cell beginning
+    if [ "$skip_next" == True ]; then
+        skip_next=False
+        continue
+    fi
+    # add indents
     if [ "$add_indent" == True ]; then
+        # don't add indents if empty line
         if [[ "$line" == *[![:space:]]* ]]; then
             line="    $line"
         fi
@@ -24,12 +39,14 @@ for line in "${file_lines[@]}"; do
     else
         script+=("$line")
     fi
+    # search for the beginning of the script
     if [ "$line" == "# !script" ]; then
         script+=("if __name__ == '__main__':")
         add_indent=True
     fi
 done
 
+# rewrite file
 for line in "${script[@]}"; do
     echo "$line" >> "$FILENAME"
 done
